99ef70ab464fec9146c517d0978a01c49844f11b
kohei1218 <yandapanda1218@gmail.com>
2018-04-26 20:46:18 +0900

    initial commit

---
+++ MyBitCheckerExtension/TodayViewController.swift
@@ -0,0 +1,47 @@
+//
+//  TodayViewController.swift
+//  MyBitCheckerExtension
+//
+//  Created by 齋藤　航平 on 2018/04/26.
+//  Copyright © 2018年 齋藤　航平. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+    
+    @IBOutlet weak var totalAssetLabel: UILabel!
+    @IBOutlet weak var BtcLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.extensionContext?.widgetLargestAvailableDisplayMode = NCWidgetDisplayMode.expanded
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+    func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize){
+        if (activeDisplayMode == NCWidgetDisplayMode.compact) {
+            self.preferredContentSize = maxSize;
+        }
+        else {
+            self.preferredContentSize = CGSize(width: 0, height: 100);
+        }
+    }
+    
+}
