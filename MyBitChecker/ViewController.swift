e366557516291bb504ed632fb48fe32f0813328c
kohei1218 <yandapanda1218@gmail.com>
2018-04-26 20:28:49 +0900

    Initial Commit

---
+++ MyBitChecker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyBitChecker
+//
+//  Created by 齋藤　航平 on 2018/04/26.
+//  Copyright © 2018年 齋藤　航平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
